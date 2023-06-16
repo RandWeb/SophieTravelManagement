6d31d3354122efa8e274f68498c87bc27aa92403
مهرداد تابش <mehrdadit12@gmail.com>
2023-06-16 08:57:09 -0700

    اضافه کردن کنترلر

--- SophieTravelManagement.Api/Controllers/TravelerCheckListController.cs
+++ SophieTravelManagement.Api/Controllers/TravelerCheckListController.cs
@@ -1,4 +1,8 @@
-﻿using SophieTravelManagement.Shared.Abstractions.Commands;
+﻿using Microsoft.AspNetCore.Mvc;
+using SophieTravelManagement.Application.Commands;
+using SophieTravelManagement.Application.Dto;
+using SophieTravelManagement.Application.Queries;
+using SophieTravelManagement.Shared.Abstractions.Commands;
 using SophieTravelManagement.Shared.Abstractions.Queries;
 
 namespace SophieTravelManagement.Api.Controllers;
@@ -14,5 +18,53 @@ public class TravelerCheckListController:BaseController
         _queryDispatcher = queryDispatcher;
     }
     
-    
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<TravelerCheckListDto>> Get([FromRoute] GetTravelerCheckList query)
+    {
+        var result = await _queryDispatcher.QueryAsync(query);
+        return OkOrNotFound(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TravelerCheckListDto>>> Get([FromQuery] SearchTravelerCheckList query)
+    {
+        var result = await _queryDispatcher.QueryAsync(query);
+        return OkOrNotFound(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] CreateTravelerCheckListWithItemsCommand command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return CreatedAtAction(nameof(Get), new { id = command.Id }, null);
+    }
+
+    [HttpPut("{TravelerCheckListId}/items")]
+    public async Task<IActionResult> Put([FromBody] AddTravelerItemCommand command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
+    }
+
+    [HttpPut("{TravelerCheckListId:guid}/items/{name}/Take")]
+    public async Task<IActionResult> Put([FromBody] TakeItemCommand command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
+    }
+
+    [HttpDelete("{TravelerCheckListId:guid}/items/{name}")]
+    public async Task<IActionResult> Delete([FromBody] RemoveTravelerItemCommand command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete([FromBody] RemoveTravelerCheckListCommand command)
+    {
+        await _commandDispatcher.DispatchAsync(command);
+        return Ok();
+    }
+
 }
